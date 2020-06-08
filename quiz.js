662374885fb40c3a5673e34a103b92165d1bf768
josedanielasn <63945372+josedanielasn@users.noreply.github.com>
2020-06-08 19:28:18 +0800

    removed random comments

--- quiz.js
+++ quiz.js
@@ -37,51 +37,67 @@ function questions(question, choices, answer) {
   };
 }
 
+questions.prototype.select = function () {
+  let answer = window.prompt('Number corresponding to your answer:');
+  // if (1 === this.choices.indexof){}
+  let correct_num = this.choices.indexOf(this.answer) + 1;
+  // console.log(answer);
+  if (answer == correct_num) {
+    console.log('Your answer is CORRECT!');
+    score = score + 1;
+    console.log('Your score is now = ' + score);
+    if (set.length === 0) {
+      console.log('Congratulations, you finished the quiz.');
+    } else {
+      gen_question();
+    }
+  } else if (set.length !== correct_num) {
+    console.log('Your answer is WRONG.');
+    if (set.length === 0) {
+      console.log('Congratulations, you finished the quiz.');
+    } else {
+      gen_question();
+    }
+  }
+};
+
 function list_questions() {
   let num_1 = new questions(
-    // 1,
     'What is the biggest planet in our solar system?',
     ['Earth', 'Jupiter', 'Saturn', 'Uranus'],
     'Jupiter'
   );
   let num_2 = new questions(
-    // 2,
     'What is the chemical symbol for the element oxygen?',
     ['H', 'N', 'O', 'Ox'],
     'O'
   );
   let num_3 = new questions(
-    // 3,
     'What is the 7th element on the periodic table of elements?',
     ['Nitrogen', 'Oxygen', 'Carbon', 'Helium'],
     'Nitrogen'
   );
   let num_4 = new questions(
-    // 4,
     'The highest mountain on earth is?',
     ['Mt.Apo', 'Mt. Ridge', 'Mt.Hieghts', 'Mt.Everest'],
     'Mt.Everest'
   );
   let num_5 = new questions(
-    // 5,
     'The fear of what animal is known as ‘arachnophobia’?',
     ['Flies', 'Cockroach', 'Spider', 'Grasshopper'],
     'Spider'
   );
   let num_6 = new questions(
-    // 6,
     'What is the name of the long appendage that hangs from an elephants face?',
     ['Tail', 'Nose', 'Ears', 'Tongue'],
     'Nose'
   );
   let num_7 = new questions(
-    // 7,
     'What is the name of the part of the human skeleton which protects our brain?',
     ['Collar Bone', 'Clavicle', 'Ribs', 'Skull'],
     'Skull'
   );
   let num_8 = new questions(
-    // 8,
     'What is the name of the closest star to the earth?',
     ['Milkyway', 'Andromeda', 'The Sun', 'The Moon'],
     'The Sun'
